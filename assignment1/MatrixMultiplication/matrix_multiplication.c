6066589d1ac11f661acea26dc5e9cf11f8011021
Shuo Sun <petersun116@gmail.com>
2019-10-14 23:30:22 +0200

    Implemented blocked matrix multiplication.

--- assignment1/MatrixMultiplication/matrix_multiplication.c
+++ assignment1/MatrixMultiplication/matrix_multiplication.c
@@ -14,12 +14,18 @@ void multiply_naive(double *a, double *b, double *c, int n) {
 
 /* Multiply n x n matrices a and b  */
 void multiply_blocked(double *a, double *b, double *c, int n, int B) {
-//  int i, j, k, i1, j1, k1;
-//  for (i = 0; i < n; i+=B)
-//    for (j = 0; j < n; j+=B)
-//      for(k = 0; k < n; k+=B)
-//        /* B x B mini matrix multiplications */
-//        // TODO: Complete this method
+  int i, j, k, i1, j1, k1;
+  for (i = 0; i < n; i+=B)
+    for (j = 0; j < n; j+=B)
+      for(k = 0; k < n; k+=B)
+        /* B x B mini matrix multiplications */
+        // DONE: Complete this method
+        for (i1 = 0; i1 < B; i1++)
+          for (j1 = 0; j1 < B; j1++)
+            for(k1 = 0; k1 < B; k1++)
+            {
+              c[n*(i+i1)+(j+j1)] += a[n*(i+i1)+(k+k1)] * b[n*(k+k1)+(j+j1)];
+            }
 }
 
 void readMatrix(char* filename, double* matrix, int n) {
