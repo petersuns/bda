4de11d583e2300df03380a9f5173181a39d1ca01
Shuo Sun <r0689323@ham.cs.kotnet.kuleuven.be>
2019-11-09 13:12:30 +0100

    First basic version of mulmatrix.py.
    Not yet blocked, not yet plot.

--- assignment1/MatrixMultiplication/mulmatrix.py
+++ assignment1/MatrixMultiplication/mulmatrix.py
@@ -3,6 +3,7 @@
 # param 2-N: different block sizes
 
 import sys
+import subprocess
 
 file_name = sys.argv[1]
 block_sizes = map(lambda block_size: int(block_size), sys.argv[2:])
@@ -14,3 +15,23 @@ print("The result of this script is a graph 'graph.png' in the current",
       "directory that shows the results with matrix size on the x-axis and",
       "time on the y-axis. Create one line for each block size and one line",
       "for the naive strategy.")
+
+method = "naive"
+
+fd_in = open(file_name, "r")
+for line in fd_in.readlines():
+    print("Line: {}".format(line))
+    arguments = line.split()
+    fd_out = open("output_{}_{}_{}_{}".format(method, arguments[0], arguments[1], arguments[2]), "w+")
+    subprocess.call(
+        "./matrix_multiplication {} {} {} {}".format(
+            method,
+            arguments[0],
+            arguments[1],
+            arguments[2]),
+        stdin=None,
+        stdout=fd_out,
+        stderr=fd_out,
+        shell=True)
+    fd_out.close()
+fd_in.close()
