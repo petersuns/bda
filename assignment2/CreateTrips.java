3c99be4913780129bbab4ecf02f59b6ea3db1a65
Shuo Sun <petersun116@gmail.com>
2020-01-30 22:55:15 +0100

    First version, dummy implementation.

--- assignment2/CreateTrips.java
+++ assignment2/CreateTrips.java
@@ -1,55 +1,57 @@
 import java.io.IOException;
-import java.nio.file.Path;
+
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.w3c.dom.Text;
 
-import jdk.internal.module.ModuleLoaderMap.Mapper;
+public class CreateTrips {
 
-public class WordCount {
-
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable> {
+    public static class SegmentMapper extends Mapper<Object, Text, Text, IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+        private Text segment = new Text();
 
-        private Pattern punctuationPattern = Pattern.compile("\\p{P}");
-        private Pattern wordPattern = Pattern.compile("[a-z]+");
+        // private Pattern punctuationPattern = Pattern.compile("\\p{P}");
+        // private Pattern wordPattern = Pattern.compile("[a-z]+");
 
-        public void map(Object key, Text value, Context context)
-                throws IOException, InterruptedException {
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
 
             while (itr.hasMoreTokens()) {
-                String token = itr.nextToken().toLowerCase();             // convert words to lowercase
-                token = punctuationPattern.matcher(token).replaceAll(""); // remove punctuation
-                if (wordPattern.matcher(token).matches()) {               // remove non-latin-words
-                    word.set(token);
-                    context.write(word, one);
-                }
+                String token = itr.nextToken(); // .toLowerCase(); // convert words to lowercase
+                segment.set(token);
+                context.write(segment, one);
+                // token = punctuationPattern.matcher(token).replaceAll(""); // remove
+                // punctuation
+                // if (wordPattern.matcher(token).matches()) { // remove non-latin-words
+                // word.set(token);
+                // context.write(word, one);
+                // }
+
             }
         }
     }
 
-    public static class IntSumReducer
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class DummyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
         private IntWritable result = new IntWritable();
 
         public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
             int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
+            // for (IntWritable val : values) {
+            // sum += val.get();
+            // }
 
             result.set(sum);
             context.write(key, result);
@@ -58,12 +60,12 @@ public class WordCount {
 
     /* --- main methods ---------------------------------------------------- */
 
-    public static Job runWordCount(Path input, Path output) throws Exception {
+    public static Job runCreateTrips(Path input, Path output) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setReducerClass(IntSumReducer.class);
+        job.setJarByClass(CreateTrips.class);
+        job.setMapperClass(SegmentMapper.class);
+        job.setReducerClass(DummyReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         job.setOutputFormatClass(TextOutputFormat.class);
@@ -77,10 +79,9 @@ public class WordCount {
         Path input = new Path(args[0]);
         Path output1 = new Path(args[1], "pass1");
 
-        Job wordcountJob = runWordCount(input, output1);
-        if (!wordcountJob.waitForCompletion(true)) {
+        Job createTripsJob = runCreateTrips(input, output1);
+        if (!createTripsJob.waitForCompletion(true)) {
             System.exit(1);
         }
-
     }
 }
